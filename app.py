9c7156079ab6c5c1cee711f5971c2717a581ec60
Samuel Bancroft <spiruel@gmail.com>
2022-10-27 13:45:29 +0100

    analytics added

--- app.py
+++ app.py
@@ -4,14 +4,10 @@ import streamlit_book as stb
 import geemap
 from pathlib import Path
 #geemap.ee_initialize()
-
-
+import os
 st.session_state["warned_about_save_answers"] = True
 
-
 st.set_page_config(layout="wide", page_title="SatSchool", page_icon="🛰️")
-
-
 
 hide_streamlit_style = """
 <style>
@@ -27,39 +23,45 @@ current_path = Path(__file__).parent.absolute()
 
 # Streamit book properties
 stb.set_book_config(menu_title="Main Menu",
-                    menu_icon="",
-                    options=[
-                            "Introduction",
-                            "Land",
-                            "Oceans",
-                            "Ice",
-                            "Quiz"
-                            ],
-                    paths=[
-                        current_path / "apps/intro",
-                        current_path / "apps/land",
-                        current_path / "apps/oceans",
-                        current_path / "apps/ice.py",
-                        current_path / "apps/quiz.py",
-                          ],
-                    icons=[
-                          "house",
-                          "",
-                          "",
-                          "",
-                          "",
-                          "trophy"
-                          ],
-                    save_answers=True,
-                    )
+                menu_icon="",
+                options=[
+                        "Introduction",
+                        "Land",
+                        "Oceans",
+                        "Ice",
+                        "Quiz"
+                        ],
+                paths=[
+                    current_path / "apps/intro",
+                    current_path / "apps/land",
+                    current_path / "apps/oceans",
+                    current_path / "apps/ice.py",
+                    current_path / "apps/quiz.py",
+                        ],
+                icons=[
+                        "house",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "trophy"
+                        ],
+                save_answers=True,
+                )
     
 with st.sidebar:
 
     st.sidebar.title("About")
     st.sidebar.info(
-        """
-        🌐 https://eo-cdt.org
-        
-        ©️ 2022 SatSchool
+    f"""
+    🌐 https://eo-cdt.org
+    
+    ©️ 2022 SatSchool
     """
     )
+
+    if 'analytics' in st.experimental_get_query_params().keys():
+        if st.experimental_get_query_params()['analytics'] == ['on']:
+            stb.admin.configuration()
+            stb.admin.user_info()
+            stb.admin.answer_info()
