8c356e38396cf945d9d0dfe9590aa287e5eb981b
Samuel Bancroft <spiruel@gmail.com>
2023-06-08 16:54:26 +0100

    Update and rename 01 ndvi.py to 02 ndvi.py

--- apps/land/02 ndvi.py
+++ apps/land/02 ndvi.py
@@ -11,15 +11,13 @@ st.title('Global Normalised Difference Vegetation Index for 2013')
 m = geemap.Map(height=800)
 
 dataset = ee.ImageCollection('NASA/GIMMS/3GV0').filter(ee.Filter.date('2013-06-01', '2013-12-31'))
-dataset_ndvi = dataset2019.select('ndvi')
-
-ndvi = dataset.select('ndvi');
+ndvi = dataset.select('ndvi')
 ndviVis = {
   'min': -1.0,
   'max': 1.0,
   'palette': ['000000', 'f5f5f5', '119701'],
 }
-m.setCenter(-88.6, 26.4, 1);
-m.addLayer(ndvi, ndviVis, 'NDVI');
+m.setCenter(-88.6, 26.4, 1)
+m.addLayer(ndvi, ndviVis, 'NDVI')
 
 m.to_streamlit()
