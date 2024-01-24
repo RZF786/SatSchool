6f237b3df2a0f61f7cfb85fbaf329f42dfc37be7
Samuel Bancroft <spiruel@gmail.com>
2024-01-24 15:50:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 import streamlit_book as stb
 import geemap
 from pathlib import Path
-import ee
-ee.Initialize(project='ee-spiruel')
+geemap.ee_initialize(**{'project'='ee-spiruel'})
 import os
 st.session_state["warned_about_save_answers"] = True
 
